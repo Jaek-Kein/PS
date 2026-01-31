e05fd85b6a34a4c503ae0278cce97ae724cea805
Jaewook Kim / Kein <103343846+Jaek-Kein@users.noreply.github.com>
2026-01-31 11:50:37 +0900

    [Silver IV] Title: 덱, Time: 188 ms, Memory: 13080 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.js
@@ -0,0 +1,40 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const input = [];
+
+rl.on("line", (line) => {
+    input.push(line.trim());
+}).on("close", () => {
+    const lines = Number(input[0]);
+    const stack = [];
+    const out = [];
+
+    for (let i = 1; i <= lines; i++) {
+        const [command, argument] = input[i].split(" ");
+
+        if (command === "push_front") {
+            stack.unshift(argument);
+        } else if (command === "push_back") {
+            stack.push(argument);
+        } else if (command === "pop_front") {
+            out.push(stack.length ? stack.shift() : -1);
+        } else if (command === "pop_back") {
+            out.push(stack.length ? stack.pop() : -1);
+        } else if (command === "size") {
+            out.push(stack.length);
+        } else if (command === "empty") {
+            out.push(stack.length ? 0 : 1);
+        } else if (command === "front") {
+            out.push(stack.length ? stack[0] : -1);
+        } else if (command === "back") {
+            out.push(stack.length ? stack[stack.length -1] : -1);
+        } 
+    }
+
+    console.log(out.join("\n"));
+});
